f5c635d0ddcc9712b8e239ab789c211d9d5689e2
Decorcril <githubdecorcril@gmail.com>
2026-04-30 09:32:23 -0300

    feat: adiciona paginação a lista de produtos

--- produtos/views/produto.py
+++ produtos/views/produto.py
@@ -1,34 +1,43 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.db.models.deletion import ProtectedError
 from core.mixins import estoquista_ou_admin
 from produtos.models import Produto
 from produtos.forms import ProdutoForm
-from django.db.models.deletion import ProtectedError
+from estoque.models import Estoque
 
 
 @login_required
 def produto_list(request):
     q = request.GET.get('q', '')
     categoria = request.GET.get('categoria', '')
+
     produtos = Produto.objects.all()
 
     if q:
-        from django.db.models import Q
         produtos = produtos.filter(
             Q(nome__icontains=q) | Q(codigo__icontains=q)
         )
     if categoria:
         produtos = produtos.filter(categoria=categoria)
 
-    from estoque.models import Estoque
-    for produto in produtos:
+    produtos = produtos.order_by('nome')
+
+    paginator = Paginator(produtos, 24)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    for produto in page_obj:
         produto.saldos_por_local = Estoque.objects.filter(
             produto=produto, quantidade__gt=0
         ).select_related('local')
 
     return render(request, 'produtos/produto/list.html', {
-        'produtos': produtos,
+        'produtos': page_obj,
+        'page_obj': page_obj,
         'q': q,
         'categoria': categoria,
         'categoria_choices': Produto.CATEGORIA_CHOICES,
