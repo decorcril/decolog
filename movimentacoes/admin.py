c19f684e059b75b3a366832c05779201ab6c0173
Decorcril <githubdecorcril@gmail.com>
2026-06-10 09:09:09 -0300

    fix: registra movimentacao no admin para permitir exclusão

--- movimentacoes/admin.py
+++ movimentacoes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from movimentacoes.models import Movimentacao
 
-# Register your models here.
+@admin.register(Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display = ['data_hora', 'produto', 'tipo', 'motivo', 'quantidade', 'local', 'usuario']
+    list_filter = ['tipo', 'motivo']
+    search_fields = ['produto__nome', 'produto__codigo']
+    ordering = ['-data_hora']
